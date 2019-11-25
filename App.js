2aaf8147a56d13645df2388992fa5e7d9ee84070
sandieguitoacademyapp <56521621+sandieguitoacademyapp@users.noreply.github.com>
2019-11-24 17:56:03 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,65 @@
-import React, { Component } from 'react';
-import {Text, TextInput, View } from 'react-native';
-import * as RNFS from 'react-native-fs';
+//imports
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+//Screen Indicator
+let onScreen = "Main";
+
+//Tree Planting
+var tree = new Map();
+tree.set("Main",
+<Screen
+parent = "Main"
+children = {[]}
+view =
+<Text>Hi</Text>
+/>);
+
+//Tree Children Update
+tree.forEach(setChild);
+function setChild(value, key, map) {
+  tree.get(tree.get(key).props.parent).props.children.push(key);
+}
+
+//App class
+export default class App extends React.Component {
+
+  //render
+  render() {
+    return (
+
+      //Display the indicated Screen
+      <Screen parent = {tree.get(onScreen).props.parent}
+      view =
+      {tree.get(onScreen).props.view}
+      />
+    );
+  }
+}
+
+//Screen class
+class Screen extends React.Component {
+
+  //constructor
+  constructor(props)
+  {
+    super(props);
+  }
+
+  //render
+  render() {
+    return (
+
+      //Display the view that is passed in
+      <View>
+        {this.props.view}
+      </View>
+    );
+  }
+
+  //Switch to another Screen
+  toScreen(to)
+  {
+    onScreen = to;
+  }
+}
